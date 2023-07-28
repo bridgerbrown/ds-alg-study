729c24e5d7fdffa63ec1acf48070b4a72e5d1293
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-28 15:52:09 -0700

    arrays, class

--- arrays.js
+++ arrays.js
@@ -18,3 +18,93 @@ strings.unshift('x'); // O(n): it requires iterating through to reassign indices
 // splice: remove and/or add item from specific index
 strings.splice(2, 0, 'alien'); // O(n/2) or O(n)
 
+
+/*
+ * CLASSES IN JS
+*/
+
+// reference type
+var object1 = { value: 10};
+var object2 = object1;
+var object3 = { value: 10 };
+
+// context vs scope
+const object4 = {
+  a: function() {
+    console.log(this);
+  }
+}
+
+// instantiation
+class Player {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  introduce() {
+    console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+  }
+}
+
+class Wizard extends Player {
+  constructor(name, type) {
+    super(name, type)
+  }
+  play() {
+    console.log(`I'm a ${this.type}`);
+  }
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Shawn', 'Dark Magic')
+
+
+/*
+ * IMPLEMENTING AN ARRAY
+*/
+const simpleArray = [];
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index]
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push('hi');
+newArray.push('you');
+newArray.push('1');
+newArray.pop();
+newArray.delete(0);
