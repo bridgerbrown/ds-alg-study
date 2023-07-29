28c6e64bf047cc2e3565ae562e875684ce2b1f3d
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-07-28 18:59:46 -0700

    array exercises

--- arrays.js
+++ arrays.js
@@ -108,3 +108,83 @@ newArray.push('you');
 newArray.push('1');
 newArray.pop();
 newArray.delete(0);
+
+/*
+ * Exercise: Reverse a String
+*/
+function reverse(str) {
+  if (!str || str.length < 2 || typeof str !== 'string') return
+
+  let reversed = [];
+  for (let i = 0; i < str.length; i--) {
+    reversed.push(str[i]);
+  }
+  return reversed.join('');
+};
+
+function reverse2(str) {
+  return [...str].reverse().join('');
+}
+
+/*
+ * Exercise: Merge Sorted Arrays 
+*/
+function mergeSortedArrays(array1, array2){
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+  
+  if(array1.length === 0) {
+    return array2;
+  }
+  if(array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item){
+   if(array2Item === undefined || array1Item < array2Item){
+     mergedArray.push(array1Item);
+     array1Item = array1[i];
+     i++;
+   }   
+   else {
+     mergedArray.push(array2Item);
+     array2Item = array2[j];
+     j++;
+   }
+  }
+  return mergedArray;
+}
+
+mergeSortedArrays([0,3,4,31], [3,4,6,30]);
+
+/*
+ * Exercise: Two Sum
+*/
+var twoSum = function(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i]; 
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+}; // O(n)
+
+/*
+ * Exercise: Maximum Subarray
+*/
+var maxSubArray = function(nums) {
+  let maxSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = Math.max(0, nums[i - 1] + nums[i])
+    if (nums[i] > maxSum) {
+      maxSum = nums[i];
+    }
+  }
+  return maxSum;
+}
